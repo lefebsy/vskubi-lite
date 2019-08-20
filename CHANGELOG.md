3839e9510d61350567e12c67c53f0700e240e13d
fidjiimail@gmail.com <fidjiimail@gmail.com>
2019-08-20 22:50:05 +0200

    v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to the "vskubi-lite" extension will be documented in this file.
 
-## [Unreleased]
+## 0.0.2
+- Some fixes and new option to use the token generation
 
+## 0.0.1
 - Initial release - LIte version from VSKubi, compatible Theia
--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,14 @@ import * as vscode from 'vscode';
 import * as child_process from "child_process";
 
 export function activate(context: vscode.ExtensionContext) {
+    const kubiOutputChannel: vscode.OutputChannel = vscode.window.createOutputChannel('Kubi');
+
 	let disposable = vscode.commands.registerCommand('extension.vskubi-lite', () => {
 		const kubiPath = vscode.workspace.getConfiguration('Kubi').get('path');
         const kubiEndpoint = vscode.workspace.getConfiguration('Kubi').get('endpoint');
         const kubiLogin = vscode.workspace.getConfiguration('Kubi').get('login');
+        const kubiAction = vscode.workspace.getConfiguration('Kubi').get('action');
+        const kubiExtra = vscode.workspace.getConfiguration('Kubi').get('extra');
 
 		vscode.window.showInputBox({
             prompt: "Password ?",
@@ -13,22 +17,32 @@ export function activate(context: vscode.ExtensionContext) {
             password: true,
             ignoreFocusOut: true,
         }).then((kubiPwd?: string) => {
-            //entering pwd promise
+            //entering pwd modalbox promise
+
             if (!kubiPwd) {
-                return;
+                return; //quit if no password
             }
+
             // building command
-            var kubiCommand = `${kubiPath} --username ${kubiLogin} --password ${kubiPwd} --kubi-url ${kubiEndpoint} --generate-config`;
+            let kubiCommand = `${kubiPath} ${kubiExtra} --username ${kubiLogin} --password ${kubiPwd} --kubi-url ${kubiEndpoint} --${kubiAction}`;
 
             //spawning child processus
             child_process.exec(kubiCommand, (err, stdout) => {
                 if (err) {
                     console.error(err);
-                    vscode.window.showErrorMessage(stdout);
+                    vscode.window.showErrorMessage(`${kubiLogin} : ${stdout}`);
+                    if (kubiAction === "generate-token") {
+                        kubiOutputChannel.appendLine("Generated token :");
+                        kubiOutputChannel.appendLine(stdout);
+                    }
                     return;
                 }
                 if (stdout) {
                     vscode.window.showInformationMessage(stdout);
+                    if (kubiAction === "generate-token") {
+                        kubiOutputChannel.appendLine("Generated token :");
+                        kubiOutputChannel.appendLine(stdout);
+                    }
                 }
             });
         });
